0871127099423f5666427341ccedb25295595842
ahmedehab311 <ahmedehab.aa47@gmail.com>
2024-07-18 15:34:11 +0300

    update

---
+++ typeScript/src/components/Register.tsx
@@ -0,0 +1,8 @@
+import { useRef,useState,useEffect } from "react";
+import { faCheck,faTimes,faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+
+function Register() {
+  return <h1>Registesdfsdr</h1>;
+}
+
+export default Register;
